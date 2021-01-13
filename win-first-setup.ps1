8f0f4bb2973dd295964775a6a35c4d4cef2937a8
dmitriy.grebenshikov <dmitriy.grebenshikov@travelline.ru>
2021-01-13 17:02:54 +0300

    Initial commit

---
+++ win-first-setup.ps1
@@ -0,0 +1,10 @@
+#install chocolatey repo
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+#install programs from chocolatey
+$list = @("googlechrome","7zip","adobereader","sharex","vlc")
+foreach ($program in $list) {
+    choco install $program -y --force
+}
+
+choco install office2019proplus --params '/Language:ru-ru'
