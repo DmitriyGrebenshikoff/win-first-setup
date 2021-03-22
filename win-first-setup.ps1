356d4eaae849daa86de57336078764a3c8122f41
dmitriy.grebenshikov <dmitriy.grebenshikov@travelline.ru>
2021-03-22 17:54:56 +0300

    Add ubuntu,git,keepass and enable sshagent service

--- win-first-setup.ps1
+++ win-first-setup.ps1
@@ -8,9 +8,13 @@ Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
 Get-WindowsCapability -Name RSAT* -Online | Add-WindowsCapability –Online
 
 #install programs from chocolatey
-$list = @("googlechrome","7zip","adobereader","sharex","vlc","vscode","telegram","whatsapp","viber","wsl","microsoft-teams","filezilla","putty","sql-server-management-studio","heidisql","far","microsoft-windows-terminal","winbox","puntoswitcher")
+$list = @("googlechrome","7zip","adobereader","sharex","vlc","git","keepass","vscode","telegram","whatsapp","viber","wsl","wsl-ubuntu-2004","microsoft-teams","filezilla","putty.install","sql-server-management-studio","heidisql","far","microsoft-windows-terminal","winbox","puntoswitcher")
 foreach ($program in $list) {
     choco install $program -y --force
 }
 
 choco install office365proplus --params '/Language:ru-ru' -y --force
+
+#Enable windows ssh-agent for VSCode
+Set-Service ssh-agent -StartupType Automatic
+Start-Service ssh-agent
