bc0b84949c425fb49d3c9d779b0e8b03a0102753
dmitriy.grebenshikov <dmitriy.grebenshikov@travelline.ru>
2021-03-22 15:23:01 +0300

    Add config for me

--- win-first-setup.ps1
+++ win-first-setup.ps1
@@ -1,10 +1,16 @@
 #install chocolatey repo
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
+#Install Hyper-V
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
+
+#Install RSAT
+Get-WindowsCapability -Name RSAT* -Online | Add-WindowsCapability –Online
+
 #install programs from chocolatey
-$list = @("googlechrome","7zip","adobereader","sharex","vlc")
+$list = @("googlechrome","7zip","adobereader","sharex","vlc","vscode","telegram","whatsapp","viber","wsl","microsoft-teams","filezilla","putty","sql-server-management-studio","heidisql","far","microsoft-windows-terminal","winbox","puntoswitcher")
 foreach ($program in $list) {
     choco install $program -y --force
 }
 
-choco install office2019proplus --params '/Language:ru-ru' -y --force
+choco install office365proplus --params '/Language:ru-ru' -y --force
